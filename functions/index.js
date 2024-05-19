c7d0c47d02d7bedb358b7b5bc0f159a4a36ee9cf
vaibhav vaish <vaibhavvaish@vaibhavs-MacBook-Air.local>
2024-05-19 22:38:49 +0530

    initial_commit

---
+++ functions/index.js
@@ -0,0 +1,41 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+
+admin.initializeApp();
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+exports.sendFeedbackEmail = functions.firestore
+    .document("feedback/{feedbackId}")
+    .onCreate((snap, context) => {
+      const feedbackData = snap.data();
+
+      const mailOptions = {
+        from: gmailEmail,
+        // eslint-disable-next-line max-len
+        to: "devvaibhav943@gmail.com", // Change to your recipient email address
+        subject: "New Hospital Feedback Received",
+        // eslint-disable-next-line max-len
+        text: `Feedback received from ${feedbackData.name} (${feedbackData.email}):
+Rating: ${feedbackData.rating}
+Feedback: ${feedbackData.feedback}`,
+      };
+
+      return transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error("Error sending email:", error);
+          return;
+        }
+        console.log("Email sent:", info.response);
+      });
+    });
